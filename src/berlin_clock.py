85e78378309de5a92c98afd2e5f23fa3fda49418
Hannah Mehravari <hannah.mehravari@res-group.com>
2020-02-14 10:45:29 +0000

    Refactor into smaller functions

--- src/berlin_clock.py
+++ src/berlin_clock.py
@@ -5,15 +5,45 @@ def tell_the_time_berlin(time_string):
     hours = time_list[0]
     mins = time_list[1]
     secs = time_list[2]
+    
+    row1 = get_row_1(secs)
 
-    row1 = int(hours/5)
-    row2 = hours % 5
-    row3 = int(mins/5)
-    row4 = mins % 5
-    row_big_round_light = (mins % 2 == 0)
-
-    output_string = ''
+    row2 = get_row_2(hours)
+    row3 = get_row_3(hours)
+    row4 = get_row_4(mins)
+    row5 = get_row_5(mins)
+    
+    list_of_rows =[row1, row2, row3, row4, row5]
+    
+    output_string = "\n".join(list_of_rows)
 
     return output_string
 
 
+def get_row_1(seconds):
+    no_of_lights = mins % 2
+    return create_row_string(1, no_of_lights, 'Y')
+
+def get_row_2(hours):
+    no_of_lights = int(hours/5)
+    return create_row_string(4, no_of_lights, 'R')
+
+def get_row_3(hours):
+    no_of_lights = hours % 5
+    return create_row_string(4, no_of_lights, 'R')
+
+def get_row_4(minutes):
+    no_of_lights = int(mins/5)
+    return create_row_string(11, no_of_lights, 'Y')
+
+def get_row_5(minutes):
+    no_of_lights = mins % 5
+    return create_row_string(4, no_of_lights, 'Y')
+
+def create_row_string(length, number_to_fill, filler_character):
+    row_list = ["O"] * length
+    row_list[:number_to_fill] = filler_character
+    return "".join(row_list)
+    
+    
+    
